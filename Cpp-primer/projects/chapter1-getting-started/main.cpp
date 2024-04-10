626ba19d936ee7071d6fbee705b0af5cf7b89130
Tran Quang Dai <tranquangdai7a@gmail.com>
2024-04-10 09:19:34 +0700

    update chapter 2 part 3

--- Cpp-primer/projects/chapter1-getting-started/main.cpp
+++ Cpp-primer/projects/chapter1-getting-started/main.cpp
@@ -6,8 +6,11 @@
 int main()
 {
     int value = 0, sum = 0;
-    while (std::cin >> value)   // end-of-file is Ctrl + Z
+
+    while (std::cin >> value)   // read unknown number of inputs, end-of-file is Ctrl + Z/D 
         sum += value;
-    std::cout << sum << std::endl;
+
+    std::cout << sum << std::endl;  // flush buffer, useful for debugging
+    
     return 0;
 }
--- Cpp-primer/projects/chapter2-variables-and-basic-types/main.cpp
+++ Cpp-primer/projects/chapter2-variables-and-basic-types/main.cpp
@@ -2,15 +2,26 @@
 
 int main()
 {
-    int i = 0;
+    // for integer arithmetic
+    int i = 0;  // long often = int
     long long j = 0;
-    double k = 3.14L;
+    // for floating-point computations
+    double k = 3.14L;   // instead of float or long double
+    // 2 adjacent string literals concatenated
     std::cout << "Hello world"
               << ", I'm Dai" << std::endl;
+    // list initialization for single variable
     int a{0}, b(0), c = {0};
+    // separate compilation
     extern int d;
+    // reference
     int &refVal = a;
+    // null pointer
     int *p = nullptr;
+    // void pointer to deal with memory as memory
     void *p1;
+    // reference to pointer
+    int *&r = p;
+
     return 0;
 }
