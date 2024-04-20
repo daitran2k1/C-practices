c7a6a320a9c909712a68ca20e0930b303fca38e1
Tran Quang Dai <tranquangdai7a@gmail.com>
2024-04-20 21:45:39 +0700

    update chapter 7 exercises part 1

---
+++ Cpp-primer/projects/chapter7-classes/ex7.1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "Sales_data.h"
+
+int main()
+{
+    Sales_data currData, data;
+    double currPrice, price;
+
+    unsigned total_units_sold = 0;
+    double total_revenue = 0.0;
+
+    if (std::cin >> currData.bookNo >> currData.units_sold >> currPrice)
+    {
+        total_units_sold = currData.units_sold;
+        total_revenue = currData.units_sold * currPrice;
+
+        while (std::cin >> data.bookNo >> data.units_sold >> price)
+        {
+            if (currData.bookNo == data.bookNo)
+            {
+                total_units_sold += data.units_sold;
+                total_revenue += data.units_sold * price;
+            }
+            else
+            {
+                std::cout << currData.bookNo << " " << total_units_sold << " " << total_revenue << " "
+                          << total_revenue / total_units_sold << std::endl;
+                currData = data;
+                total_units_sold = data.units_sold;
+                total_revenue = data.units_sold * price;
+            }
+        }
+        std::cout << currData.bookNo << " " << total_units_sold << " " << total_revenue << " "
+                          << total_revenue / total_units_sold << std::endl;
+    }
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
